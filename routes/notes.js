a974130d8b9c58e513b3e0dfa960c31ba0e8ffda
Elaine Deng <qd9069@gmail.com>
2022-04-27 22:41:07 -0700

    Added API routes for notes

---
+++ routes/notes.js
@@ -0,0 +1,42 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+  
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for submitting notes
+notes.post('/', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { noteTitle, noteText } = req.body;
+  
+    // If all the required properties are present
+    if (noteTitle && noteText) {
+      // Variable for the object we will save
+      const newNote = {
+        noteTitle,
+        noteText,
+        note_id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
+  
+  module.exports = notes;
